5c91f0d022f24cd1a4f8f6ed79c3d5197d18dae6
EngAhmedMahjoub <EngAhmedMahjoub@gmail.com>
2024-05-19 06:06:47 +1000

    create login component

---
+++ src/components/Login.js
@@ -0,0 +1,43 @@
+// src/components/Login.js
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().required('Required'),
+});
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validationSchema={LoginSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          axios.post('/api/login', values).then(response => {
+            console.log(response.data);
+            setSubmitting(false);
+          }).catch(error => {
+            console.error(error);
+            setSubmitting(false);
+          });
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="email" name="email" placeholder="Email" />
+            <Field type="password" name="password" placeholder="Password" />
+            <button type="submit" disabled={isSubmitting}>
+              Login
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default Login;
