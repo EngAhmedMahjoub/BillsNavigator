41f1dab81a9f18068a6c0d6a279066bf305df94d
EngAhmedMahjoub <EngAhmedMahjoub@gmail.com>
2024-05-19 08:23:36 +1000

    route the app default page to authentication page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import AuthPage from './components/AuthPage';
+import AddExpense from './components/AddExpense';
+import ExpenseList from './components/ExpenseList';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/auth/*" element={<AuthPage />} />
+          <Route path="/add-expense" element={<AddExpense />} />
+          <Route path="/expenses" element={<ExpenseList />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Navigate to="/auth/login" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
