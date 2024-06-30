e06abae45df20bac0eeb2e123a55be907b9e4fb8
EngAhmedMahjoub <EngAhmedMahjoub@gmail.com>
2024-06-30 12:26:03 +1000

    add error message

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
@@ -11,7 +11,7 @@ const SignUpSchema = Yup.object().shape({
   
   const SignUp = () => {
     return (
-      <div className='signup-container'>
+      <div className= "signup-container">
         <h1>Sign Up</h1>
         <p>Create a new account for free.</p>
         <Formik
@@ -28,15 +28,18 @@ const SignUpSchema = Yup.object().shape({
           }}
         >
           {({ isSubmitting }) => (
-            <Form className='signup-form'>
-              <div className='form-group'>
+            <Form className="signup-form">
+              <div className="form-group">
                 <Field type="text" name="username" placeholder="Username" className="form-field"/>
+                <ErrorMessage name="username" component="div" className="error-message" />
               </div>
               <div className='form-group'>
                 <Field type="email" name="email" placeholder="Email" className="form-field"/>
+                <ErrorMessage name="email" component="div" className="error-message" />
               </div>
               <div className='form-group'>
                 <Field type="password" name="password" placeholder="Password" className="form-field"/>
+                <ErrorMessage name="password" component="div" className="error-message" />
               </div>
               <button type="submit" disabled={isSubmitting} className='submit-button'>
                 Sign Up
