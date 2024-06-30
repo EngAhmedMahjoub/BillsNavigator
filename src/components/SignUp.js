b0e48b37ae8df02cd3537753c73920aebd92bbe7
EngAhmedMahjoub <EngAhmedMahjoub@gmail.com>
2024-06-30 12:28:18 +1000

    style error message

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
+import './SignUp.css'
 
 const SignUpSchema = Yup.object().shape({
     username: Yup.string().required('Required'),
